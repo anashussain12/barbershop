1df3cd4bef80647a98059de554b1543542ea3d65
Anas Hussain <hussainanas68@gmail.com>
2025-05-13 22:58:31 +0500

    location

--- app/notryork/checkout/page.js
+++ app/notryork/checkout/page.js
@@ -688,6 +688,7 @@ export default function CheckoutPage() {
             <p className="text-gray-500">
               © 2024 MISTR BARBER. All rights reserved.
               {/* © 2024 MISTR BARBER. All rights reserved. */}
+              {/* © 2024 MISTR BARBER. All rights reserved. */}
             </p>
           </div>
         </footer>
