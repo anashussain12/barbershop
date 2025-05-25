0a59a96629c9c6efb6c69bdbbd1cee4989deb8f0
Anas Hussain <hussainanas68@gmail.com>
2025-05-25 22:41:38 +0500

    all-locations-details

--- app/page.js
+++ app/page.js
@@ -165,7 +165,7 @@ export default function Home() {
               <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
             </svg>
           </div>
-          <p className="text-2xl md:text-3xl font-light italic text-gray-200 mb-8">The attention to detail and personalized service at Mr.BARBER UNISEX is unmatched. It's more than a haircut—it&aposs an experience every gentleman deserves.</p>
+          <p className="text-2xl md:text-3xl font-light italic text-gray-200 mb-8">The attention to detail and personalized service at Mr.BARBER UNISEX is unmatched. It is more than a haircut—it&aposs an experience every gentleman deserves.</p>
           <div className="flex items-center justify-center">
             <div className="w-16 h-16 rounded-full mr-4 overflow-hidden relative">
               <ImageWithFallback
