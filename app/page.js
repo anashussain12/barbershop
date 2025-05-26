c3eabce293fc36426d2e9b7e72789186b9713aa2
Anas Hussain <hussainanas68@gmail.com>
2025-05-26 19:54:07 +0500

    all-locations-details

--- app/page.js
+++ app/page.js
@@ -185,13 +185,12 @@ export default function Home() {
       </section>
 
       {/* Footer */}
-      <div className="h-px w-full bg-gradient-to-r from-transparent via-white/20 to-transparent"></div>
       <footer className="py-12 px-6 bg-[#111111]">
-        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10">
+        <div className="max-w-7xl mx-auto flex justify-center items-center ">
           <div>
             <h3 className="text-2xl font-bold mb-4 bg-gradient-to-r from-amber-500 to-yellow-400 text-transparent bg-clip-text">MR.BARBER UNISEX SALON</h3>
             <p className="text-gray-400 mb-4">Where gentlemen receive the royal treatment.</p>
-            <div className="flex space-x-4">
+            {/* <div className="flex space-x-4">
               <a href="#" className="text-amber-500 hover:text-yellow-400">
                 <span className="sr-only">Instagram</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -204,7 +203,7 @@ export default function Home() {
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                 </svg>
               </a>
-            </div>
+            </div> */}
           </div>
           {/* <div>
             <h4 className="text-white font-bold mb-4">HOURS</h4>
@@ -214,7 +213,7 @@ export default function Home() {
               <li>Sunday: 10AM - 6PM</li>
             </ul>
           </div> */}
-          <div>
+          {/* <div>
             <h4 className="text-white font-bold mb-4">CONTACT</h4>
             <ul className="text-gray-400 space-y-2">
               <li>Email: Mr.BarberUnisexBeautySalon@gmail.com</li>
@@ -222,9 +221,9 @@ export default function Home() {
               <li>Address: 2912 Dundas St W, Toronto</li>
               
             </ul>
-          </div>
+          </div> */}
         </div>
-        <div className="mt-12 pt-8 border-t border-white/10 text-center">
+        <div className="mt-12  pt-8 border-t border-white/10 text-center">
           <p className="text-gray-500">© 2024 MR.BARBER UNISEX SALON. All rights reserved.</p>
         </div>
       </footer>
