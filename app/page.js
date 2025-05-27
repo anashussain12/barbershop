5ea6d15763efdbaa45383d00268abd9dd423c01a
Anas Hussain <hussainanas68@gmail.com>
2025-05-27 14:47:16 +0500

    slider

--- app/page.js
+++ app/page.js
@@ -42,6 +42,12 @@ export default function Home() {
     label: 'Exclusive Grooming',
   },
   {
+    image: 'highlighthair.png',
+    title: 'MR.BARBER UNISEX SALON',
+    subtitle: 'Where gentlemen and womens receive the royal treatment',
+    label: 'Exclusive Grooming',
+  },
+  {
     image: '/kids.jpg',
     title: 'MR.BARBER UNISEX SALON',
     subtitle: 'Where gentlemen and womens receive the royal treatment',
